7ee2e6071404df1cf9978268cb0d1f5726bd1fa5
Thomas Rettberg <thomas.rettberg@outlook.de>
2018-04-02 03:05:01 +0200

    New HUD Elements included.

---
+++ Assets/TheGame/Scripts/canvas/HudHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudHandler : MonoBehaviour {
+
+    [SerializeField] Image healthBar;
+
+	private void Update () {
+        PlayerBehaviour player = FindObjectOfType<PlayerBehaviour>();
+        if (player != null)
+        {
+            healthBar.fillAmount = player.GetHealth();
+        }
+	}
+}
